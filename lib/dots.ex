779cc485f9c33f85bfe5c4a7fecf63eb1c604b23
Mike Simpson <mike.simpson@scimedsolutions.com>
2014-07-25 15:46:47 -0500

    First working version

--- lib/dots.ex
+++ lib/dots.ex
@@ -1,14 +1,34 @@
 defmodule Dots do
   def setup_files(path) do
-    files = path
+    file_paths = path
     |> Path.join("dot_*")
     |> Path.wildcard
-    for file <- files do
-      IO.puts file
+    for file_path <- file_paths do
+      setup_file file_path
     end
   end
 
-  def setup_file(path) do
-    File.ln_s(path, "foo")
+  def setup_file(file_path) do
+    linked_file = file_path |> updated_path
+    linked_path = Path.join(System.user_home, linked_file)
+    File.rm(linked_path)
+    output_link_status File.ln_s(file_path, linked_path), linked_file
+  end
+
+  defp updated_path(file_path) do
+    file_path
+    |> Path.basename
+    |> change_dot
+  end
+
+  defp change_dot("dot_" <> filename), do: "." <> filename
+
+  defp output_link_status({:error, message}, linked_file) do
+    IO.puts "Error linking #{linked_file}"
+    IO.puts message
+  end
+
+  defp output_link_status(:ok, linked_file) do
+    IO.puts "Linked #{linked_file}"
   end
 end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dots.Mixfile do
 
   def project do
     [app: :dots,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 0.14.3",
      deps: deps,
      escript: escript]
